a1149768b1386c358c8a50697f846bd550fc095c
KasZaim <122385481+KasZaim@users.noreply.github.com>
2023-11-15 23:50:53 +0100

    finished About me section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 const routes: Routes = [
   {path:'header', component:HeaderComponent},
-  {path:'main-page', component:MainPageComponent}
+  {path:'main-page', component:MainPageComponent},
+  {path:'about-me', component:AboutMeComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { HeaderComponent } from './header/header.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
+import { AboutMeComponent } from './about-me/about-me.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainPageComponent
+    MainPageComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
