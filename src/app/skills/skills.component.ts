9b6309bd73d6477d97b8aaf227f3dd6e071eb81b
KasZaim <122385481+KasZaim@users.noreply.github.com>
2023-11-18 12:28:22 +0100

    added animations

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
-
+  skills = [
+    { name: 'Angular', image: './assets/img/icons/angular.png' },
+    { name: 'Typescript', image: './assets/img/icons/typescript.png' },
+    { name: 'Javascript', image: './assets/img/icons/javascript.png' },
+    { name: 'HTML', image: './assets/img/icons/html.png' },
+    { name: 'Firebase', image: './assets/img/icons/firebase.png' },
+    { name: 'GIT', image: './assets/img/icons/git.png' },
+    { name: 'CSS', image: './assets/img/icons/css.png' },
+    { name: 'Rest-API', image: './assets/img/icons/api.png' },
+    { name: 'Material Design', image: './assets/img/icons/material_design.png' },
+    { name: 'Scrum', image: './assets/img/icons/scrum.png' }
+  ];
 }
